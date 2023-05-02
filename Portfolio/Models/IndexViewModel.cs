13014867e78b28d69ff4ba35bf8b4b5b0b942101
cemdos <cem.dos@hotmail.com>
2023-05-02 21:28:17 +0300

    Add project files.

---
+++ Portfolio/Models/IndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Portfolio.Models
+{
+    public class IndexViewModel
+    {
+        public MastheadViewModel Masthead { get; set; }
+        public AboutViewModel About { get; set; }
+        public bool ShowServices { get; set; } = true;
+        public bool ShowPortfolio { get; set; } = true;
+        public bool ShowCallToAction { get; set; } = true;
+        public bool ShowContact { get; set; } = true;
+
+        public IndexViewModel(IConfiguration configuration)
+        {
+            Masthead = new MastheadViewModel(configuration);
+            About = new AboutViewModel(configuration);
+        }
+    }
+}
---
+++ Portfolio/Models/MastheadViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Portfolio.Models
+{
+    public abstract class PartialBase
+    {
+        public bool Show { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string CTAText { get; set; }
+        public string CTAUrl { get; set; }
+        public bool ShowCTA { get; set; }
+
+        public PartialBase(IConfiguration _configuration, string partial_name)
+        {
+            Title = _configuration.GetValue<string>("Settings:" + partial_name + ":Title");
+            SubTitle = _configuration.GetValue<string>("Settings:" + partial_name + ":SubTitle");
+            CTAText = _configuration.GetValue<string>("Settings:" + partial_name + ":CTA:Text");
+            CTAUrl = _configuration.GetValue<string>("Settings:" + partial_name + ":CTA:Url");
+            ShowCTA = _configuration.GetValue<bool>("Settings:" + partial_name + ":CTA:Show");
+            Show = _configuration.GetValue<bool>("Settings:" + partial_name + ":Show");
+        }
+    }
+
+    public class MastheadViewModel : PartialBase
+    {
+        public MastheadViewModel(IConfiguration _configuration) : base(_configuration, "Masthead") { }
+    }
+
+    public class AboutViewModel : PartialBase
+    {
+        public AboutViewModel(IConfiguration _configuration) : base(_configuration, "About") { }
+    }
+}
